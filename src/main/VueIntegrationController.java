ce87ab5f19b1a2401b09c10c46d479d2a275723e
PierreFrugere <pierrefrugere@gmail.com>
2017-11-03 12:54:44 +0100

    [Fix] Modifications finales avant l'oral

--- src/main/VueIntegrationController.java
+++ src/main/VueIntegrationController.java
@@ -1,6 +1,5 @@
 package main;
 
-import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -15,6 +14,12 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class VueIntegrationController implements Initializable {
+
+    private final int NB_PAS_RECTANGLES  = 400;
+
+    private final int NB_PAS_TRAPEZES  = 4096;
+
+    private final int NB_PAS_SIMPSON  = 8096;
 
     @FXML //  fx:id="cb_methode"
     private ChoiceBox<String> methodes; // Value injected by FXMLLoader
@@ -34,9 +39,6 @@ public class VueIntegrationController implements Initializable {
     @FXML //  fx:id="tf_nbPas"
     private TextField tf_nbPas; // Value injected by FXMLLoader
 
-    @FXML //  fx:id="tf_precision"
-    private TextField tf_precision; // Value injected by FXMLLoader
-
     @FXML //  fx:id="ta_resultats"
     private TextArea ta_resultats; // Value injected by FXMLLoader
 
@@ -48,114 +50,147 @@ public class VueIntegrationController implements Initializable {
 
     @FXML
     private void handleIntegrerAction(ActionEvent event) {
-        // Button was clicked, do something...
+        // Cette fonction se déclenche lors d'un clic sur le bouton
+
         // Déclaration des variables
         double borneInf,
                 borneSup,
                 nbPas,
                 precision,
+                resultatMethodeRectangle1,
                 resultatMethodeRectangle,
                 resultatMethodeTrapeze,
-                resultatMethodeSimpson,
-                resultatMethodeRomberg;
+                resultatMethodeSimpson;
 
         // Récupération des variables
         // TODO - Tester si less textField sont bien des doubles
         borneInf = Double.parseDouble(tf_borneInf.getText());
         borneSup = Double.parseDouble(tf_borneSup.getText());
-        nbPas = Double.parseDouble(tf_nbPas.getText());
-        precision = Double.parseDouble(tf_precision.getText());
+        nbPas = NB_PAS_TRAPEZES;
+//        precision = Double.parseDouble(tf_precision.getText());
 
-        resultatMethodeRectangle = methodeRectangle(borneInf, borneSup, nbPas);
-        resultatMethodeTrapeze = methodeTrapeze(borneInf, borneSup, nbPas);
-        resultatMethodeSimpson = methodeSimpson(borneInf, borneSup, nbPas);
-        resultatMethodeRomberg = methodeRomberg(borneInf, borneSup, precision);
+        // Calcul méthode rectangle
+        resultatMethodeRectangle1 = methodeRectangles(borneInf, borneSup, NB_PAS_RECTANGLES);
+//        resultatMethodeRectangle = resultatMethodeRectangle1 + 2 * precision;
+//        while (Math.abs(resultatMethodeRectangle-resultatMethodeRectangle1) >= precision) {
+//            nbPas = nbPas *2;
+//            resultatMethodeRectangle = methodeRectangles(borneInf, borneSup, NB_PAS_TRAPEZES);
+//        }
+//        resultatMethodeRectangle = methodeRectangles(borneInf, borneSup, NB_PAS_TRAPEZES);
+//        System.nanoTime();
+        resultatMethodeTrapeze = methodeTrapezes(borneInf, borneSup, NB_PAS_TRAPEZES);
+        resultatMethodeSimpson = methodeSimpson(borneInf, borneSup, NB_PAS_SIMPSON);
+        // resultatMethodeRomberg = methodeRomberg(borneInf, borneSup, precision);
 
         // Affichage des résultats
-        ta_resultats.appendText("Résultat méthode des rectangles : " + resultatMethodeRectangle + "\n");
+        ta_resultats.clear();
+//        ta_resultats.appendText("Résultat méthode des rectangles : " + resultatMethodeRectangle + "\n");
+        ta_resultats.appendText("Résultat méthode des rectangles : " + resultatMethodeRectangle1 + "\n");
+        ta_resultats.appendText("**********************************************************\n");
         ta_resultats.appendText("Résultat méthode des trapèzes : " + resultatMethodeTrapeze + "\n");
+        ta_resultats.appendText("**********************************************************\n");
         ta_resultats.appendText("Résultat méthode de Simpson : " + resultatMethodeSimpson + "\n");
-        ta_resultats.appendText("Résultat méthode de Romberg : " + resultatMethodeRomberg + "\n");
-        ta_resultats.appendText("Fonction : " + fonction(5) + "\n");
-;
+        // ta_resultats.appendText("Résultat méthode de Romberg : " + resultatMethodeRomberg + "\n");
+
     }
 
-    private double methodeRectangle(double borneInf, double borneSup, double nbPas) {
-        double aire;
-        double x;
-        double hauteur;
+    /**
+     * Calcul de l'intégrale de la fonction saisie par l'utilisateur suivant la méthode des rectangles (point milieu)
+     * @param borneInf borne inferieure d'intégration
+     * @param borneSup : borne supérieure d'intégration
+     * @param nbPas nombre de pas
+     * @return la valeur de cette intégrale
+     */
+    private double methodeRectangles(double borneInf, double borneSup, double nbPas) {
+        double aire;        // surface retournée
+        double x;           // variable de la fonction f(x)
+        double hauteur;     // hauteur du rectangle
 
-        // Initialisation des variables
+        // On initialise les variables
         aire = 0;
         x = borneInf;
-        hauteur = (borneSup-borneInf)/nbPas;
+        hauteur = (borneSup - borneInf) / nbPas;
 
-        // Boucle de calcul
+        // On effectue la boucle de calcul
         while (x < borneSup) {
-            aire = aire + hauteur * (fonction(x+hauteur)+fonction(x))/2;
-            x = x+hauteur;
+            aire = aire + hauteur * (fonction(x + hauteur) + fonction(x)) / 2;
+            x = x + hauteur;
         }
 
         return aire;
     }
 
-    private double methodeTrapeze(double borneInf, double borneSup, double nbPas) {
+    /**
+     * Calcul de l'intégrale de la fonction saisie par l'utilisateur suivant la méthode des trapèzes
+     * @param borneInf borne inferieure d'intégration
+     * @param borneSup : borne supérieure d'intégration
+     * @param nbPas nombre de pas
+     * @return la valeur de cette intégrale
+     */
+    private double methodeTrapezes(double borneInf, double borneSup, double nbPas) {
         double aire;
         double aireApproximative;
         double hauteur;
         int i;
 
-        // Initialisation des variables
+        // On initialise les variables
         aire = 0;
         hauteur = (borneSup-borneInf)/nbPas;
 
-        // Calcul de l'aire approximative du trapeze f(a) f(b)
+        // On alcule l’aire approximative du trapèze
         aireApproximative = (hauteur/2)*(fonction(borneInf)+fonction(borneSup));
 
-        // Boucle de calcul
-        for (i=0 ; i < nbPas-1 ; i++) {
+        // On effectue la boucle de calcul
+        for (i=1 ; i < nbPas-1 ; i++) {
             aire = aire + fonction(borneInf+i*hauteur);
         }
-        // Calcul final de l'aire
+        // On calcule l’aire finale
         aire = aireApproximative + aire * hauteur;
 
         return aire;
-
     }
+
+    /**
+     * Calcul de l'intégrale de la fonction saisie par l'utilisateur suivant la méthode de Simpson
+     * @param borneInf borne inferieure d'intégration
+     * @param borneSup : borne supérieure d'intégration
+     * @param nbPas nombre de pas
+     * @return la valeur de cette intégrale
+     */
     private double methodeSimpson(double borneInf, double borneSup, double nbPas) {
         double aire;
         double hauteur;
-        double sommePaire;
-        double sommeImpaire;
+        double sommePair;
+        double sommeImpair;
         int i;
 
-        // Initialisation des variables
+        // On initialise les variables
         aire = 0;
         hauteur = (borneSup-borneInf)/(nbPas*2);
-        sommePaire = 0;
-        sommeImpaire = 0;
+        sommePair = 0;
+        sommeImpair = 0;
 
-        // Calcul de la somme des indices impaires
+        // On calcule la somme des indices impairs
         for (i=1 ; i < nbPas-1 ; i++) {
-            sommeImpaire = sommeImpaire + fonction(borneInf+hauteur*2*i);
+            sommeImpair = sommeImpair + fonction(borneInf+hauteur*2*i);
         }
 
-        // Calcul de la somme des indices paires
-        for (i=0 ; i < nbPas ; i++) {
-            sommePaire = sommePaire + fonction(borneInf+hauteur*(2*i-1));
-        }
-        // Calcul final de l'aire
-        aire = hauteur * (fonction(borneInf) + fonction(borneSup) + 2*sommePaire + 4*sommeImpaire)/3;
+        // On calcule la somme des indices pairs
+        for (i=1 ; i < nbPas ; i++) {
+            sommePair = sommePair + fonction(borneInf+hauteur*(2*i-1));
+    }
+
+        // On calcule l’aire finale
+        aire = hauteur * (fonction(borneInf) + fonction(borneSup) + 2 * sommePair + 4 * sommeImpair) / 3;
 
         return aire;
-
-    }
-    private double methodeRomberg(double borneInf, double borneSup, double nbPas) {
-        // TODO - A implémenter
-        return 0.0;
-
     }
 
+    /**
+     * Permet de calculer f(x) de la fonction saisie par l'utilisateur
+     * @param x la valeur de x
+     * @return la valeur de f(x) calculée
+     */
     private double fonction(double x) {
         double result;
         Expression e = new ExpressionBuilder(tf_fonction.getText())
